bdfb9712ae701c39d706eb06c2a9388102bcbf48
“Zuzana <zuzana.moskvic@gmail.com>
2025-08-17 16:39:55 +0200

    Iteration 2

--- src/clue.js
+++ src/clue.js
@@ -7,7 +7,7 @@ const suspectsArray = [
     age: 45,
     description: "He has a lot of connections",
     image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    color: green
+    color: "green"
     
   },
   {
@@ -17,7 +17,7 @@ const suspectsArray = [
     age: 26,
     description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
     image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    color: white
+    color: "white"
   },
   {
     firstName: "Victor",
@@ -26,7 +26,7 @@ const suspectsArray = [
     age: 22,
     description: "Billionaire video game designer",
     image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-    color: purple
+    color: "purple"
   },
   {
     firstName: "Kasandra",
@@ -35,7 +35,7 @@ const suspectsArray = [
     age: 31,
     description: "She is an A-list movie star with a dark past",
     image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    color: red
+    color: "red"
   },
   {
     firstName: "Eleanor",
@@ -44,7 +44,7 @@ const suspectsArray = [
     age: 36,
     description: "She is from a wealthy family and uses her status and money to earn popularity",
     image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    color: blue
+    color: "blue"
   },
   {
     firstName: "Jack",
@@ -53,7 +53,7 @@ const suspectsArray = [
     age: 62,
     description: "He is a former football player who tries to get by on his former glory",
     image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-    color: yellow
+    color: "yellow"
   }
 ];
 
@@ -112,3 +112,20 @@ const roomsArray = [
 ];
 
 // Iteration 2 - create the mystery
+function selectRandom(array){
+  let randomElementIndex = Math.floor(Math.random() * array.length);
+  let randomElement = array[randomElementIndex];
+  return randomElement;
+}
+
+function pickMystery(){
+  let mystery = {
+    suspect: selectRandom(suspectsArray), 
+    weapon: selectRandom(weaponsArray), 
+    room: selectRandom(roomsArray)
+  };
+  return mystery;
+}
+
+console.log(pickMystery());
+
